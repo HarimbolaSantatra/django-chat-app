4c662767d88688d9e3e3ba8e7e11c696526bf72a
santatra <razafindratsimba34@gmail.com>
2023-08-04 08:07:34 +0300

    refac: static method to handle sort_chats_per_day

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-# main_app models
+
+from datetime import datetime
 
 
 class Room(models.Model):
@@ -19,3 +20,27 @@ class Chat(models.Model):
 
     def get_time(self):
         return self.date.strftime("%I:%M %p")
+
+
+    @staticmethod
+    def sort_chats_per_day(chats, current_username):
+        chat_per_day = []
+        current_day = None
+        for chat in chats:
+            isOwner = (chat.user.username == current_username)
+            class_name = "primary-message-row" if isOwner else "secondary-message-row"
+            new_chat = { 
+                "username":chat.user.username, 
+                "message":chat.message,
+                "time":chat.get_time(),
+                "class_name": class_name
+                }
+
+            # Sort chat messages per day
+            if current_day == None or current_day != chat.get_day():
+                current_day = chat.get_day()
+                chat_per_day.append({"day": current_day, "chats": [new_chat]})
+            else:
+                chat_per_day[-1]["chats"].append(new_chat)
+
+        return chat_per_day
--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 from main_app.models import Room, Chat
 
 import json
-from datetime import datetime
 
 @login_required(login_url='login')
 def index(request):
@@ -21,30 +20,10 @@ def room(request, room_name):
     username = request.session['username']
     current_room = Room.objects.get(name=room_name)
     chats = Chat.objects.filter(room=current_room)
-    chat_per_day = []
-    current_day = None
-    for chat in chats:
-        isOwner = (chat.user.username == username)
-        class_name = "primary-message-row" if isOwner else "secondary-message-row"
-        new_chat = { 
-            "username":chat.user.username, 
-            "message":chat.message,
-            "time":chat.get_time(),
-            "class_name": class_name
-            }
 
-        # Sort chat messages per day
-        if current_day == None or current_day != chat.get_day():
-            current_day = chat.get_day()
-            chat_per_day.append({"day": current_day, "chats": [new_chat]})
-        else:
-            chat_per_day[-1]["chats"].append(new_chat)
-
-    test_var = chat_per_day[-1]
     context = {
         'username': username,
         'room_name': room_name,
-        'chat_per_day': chat_per_day,
-        "test_var": test_var
+        'chat_per_day': Chat.sort_chats_per_day(chats, username)
     }
     return render(request, "room.html", context)
