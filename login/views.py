2b7324a2645baedc5b345b7c571e078cd61393c4
santatra <razafindratsimba34@gmail.com>
2023-08-12 06:19:31 +0300

    test

--- login/views.py
+++ login/views.py
@@ -3,18 +3,18 @@ from django.contrib.auth import authenticate, login, logout
 
 
 def login_page(request):
-    # We make the method's name login_page because 'login' is already taken by Django
-    if request.user.is_authenticated and request.session['username']:  # If already logged in, login page in not available anymore
-        return redirect('index')
-    if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        request.session['username'] = username
-        if user is not None:
-            # if user successfully logged in
-            login(request, user)
-            return redirect('index')
+    # # We make the method's name login_page because 'login' is already taken by Django
+    # if request.user.is_authenticated and request.session['username']:  # If already logged in, login page in not available anymore
+    #     return redirect('index')
+    # if request.method == "POST":
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
+    #     user = authenticate(request, username=username, password=password)
+    #     request.session['username'] = username
+    #     if user is not None:
+    #         # if user successfully logged in
+    #         login(request, user)
+    #         return redirect('index')
     context = {}
     return render(request, "login.html", context)
 
