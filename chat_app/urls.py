0f29c2ed5040114cd0b25262b056db1aec63e945
santatra <razafindratsimba34@gmail.com>
2023-08-13 14:27:56 +0300

    test: remove some url path

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from main_app.views import index
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
     path("", include('login.urls')),
-    path("chat/", include('main_app.urls')),
+    # path("chat/", include('main_app.urls')),
 ]
